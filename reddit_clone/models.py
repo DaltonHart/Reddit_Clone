43dc107b1860b7099a242089c828c0db225e76e7
DaltonHart <daltonvaren@gmail.com>
2018-06-08 13:27:20 -0700

    added auth users

--- reddit_clone/models.py
+++ reddit_clone/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+#customuser model
+class CustomUser(AbstractUser):
+    # First/last name is not a global-friendly pattern
+    name = models.CharField(blank=True, max_length=255)
+
+    def __str__(self):
+        return self.email
+
+#class for created pages with posts
+# class Sub_Reddit (models.Model):
+#     name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
+
+# class Post (models.Model):
+#     body = models.TextField()
+#     title = models.CharField(max_length=100)
+#     sub_reddit = models.ForeignKey(Sub_Reddit, on_delete=models.CASCADE, related_name='posts')
+
+#     def __str__(self):
+#         return self.title
+
+# class Comment (models.Model):
+#     user = 
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+#     body = models.TextField()
+#     sub_comment = models.ManyToManyField('self', related_name='sub_comment', blank=True)
+
+#     def __str__(self):
+#         return self.post
--- reddit_clone_project/settings.py
+++ reddit_clone_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reddit_clone',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'reddit_clone.CustomUser'
